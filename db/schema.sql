b7b61d21abe8c9c3a01e55fef58c27261b48c85d
chromodyne <dsinitiere@outlook.com>
2022-07-24 17:05:32 -0700

    Added foreign keys, fixed typo.

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
-CREATE TABLE departmenmt {
+CREATE TABLE department {
     id INT PRIMARY KEY NOT NULL,
     name VARCHAR(30) NOT NULL
 }; 
@@ -13,14 +13,18 @@ CREATE TABLE role {
     id INT PRIMARY KEY NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL
-
+    FOREIGN KEY (department_id)
+    REFERENCES (department(id))
+    ON DELETE SET NULL
 };
 
 CREATE TABLE employee {
     id INT PRIMARY KEY NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
+    FOREIGN KEY (role_id)
+    REFERENCES(role(id))
+    ON DELETE SET NULL,
+    -- TODO: Add mapping to the id above.
     manager_id INT
 };

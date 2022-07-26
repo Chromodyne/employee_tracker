611d916f192d3008584f0d4e50f59f1472750344
chromodyne <dsinitiere@outlook.com>
2022-07-26 13:43:59 -0700

    Selection logic implemented.

--- server.js
+++ server.js
@@ -29,6 +29,47 @@ function getInput() {
 //TODO: This function will begin the logic based on the choice the user made.
 function performActions(choice) {
 
-}
+    switch(choice.action) {
+
+        //View all departments.
+        case dbActions[0]:
+            console.log("Choice 0 selected.")
+            break;
+
+        //View all roles.
+        case dbActions[1]:
+            console.log("Choice 1 selected.")
+            break;
+
+        //View all employees.
+        case dbActions[2]:
+            console.log("Choice 2 selected.")
+            break;
+
+        //Add a department.
+        case dbActions[3]:
+            console.log("Choice 3 selected.")
+            break;
+
+        //Add a role.
+        case dbActions[4]:
+            console.log("Choice 4 selected.")
+            break;
+        
+        //Add an employee.
+        case dbActions[5]:
+            console.log("Choice 5 selected.")
+            break;
+        
+        //Update an employee role.
+        case dbActions[6]:
+            console.log("Choice 6 selected.")
+            break;
+
+        default:
+            console.error("Invalid case detected for action.");
+            break;
+    }
+}   
 
 getInput();
